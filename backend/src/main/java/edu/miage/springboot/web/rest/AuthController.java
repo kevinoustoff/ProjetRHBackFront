c6d15bcdf6d894b7e6aaf6906dd6732bfe4623b0
Sami <samiboulsnane68@gmail.com>
2025-01-11 19:25:30 +0100

    Ajout de la création de cv en ligne (ne marche pas encore)

--- backend/src/main/java/edu/miage/springboot/web/rest/AuthController.java
+++ backend/src/main/java/edu/miage/springboot/web/rest/AuthController.java
@@ -1,6 +1,7 @@
 package edu.miage.springboot.web.rest;
 
-import edu.miage.springboot.dao.entities.Role;
+import edu.miage.springboot.dao.entities.Utilisateur;
+import edu.miage.springboot.dao.repositories.UtilisateurRepository;
 import edu.miage.springboot.security.JwtService;
 import edu.miage.springboot.web.dtos.AuthRequestDTO;
 import edu.miage.springboot.web.dtos.AuthResponseDTO;
@@ -13,8 +14,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.GrantedAuthority;
 
-import java.util.Set;
+
+import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
@@ -25,17 +28,25 @@ public class AuthController {
     private AuthenticationManager authenticationManager;
     @Autowired
     private JwtService jwtService;
+    @Autowired
+    private UtilisateurRepository userRepository;
 
     @PostMapping("/login")
     public AuthResponseDTO AuthenticateAndGetToken(@RequestBody AuthRequestDTO authRequestDTO) {
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequestDTO.getUsername(), authRequestDTO.getPassword()));
         if (authentication.isAuthenticated()) {
-            Set<Role> roles = authentication.getAuthorities().stream()
-                    .map(authority -> new Role(authority.getAuthority())) // Créez une instance de Role avec le nom de l'autorité
-                    .collect(Collectors.toSet());
-            return new AuthResponseDTO(jwtService.GenerateToken(authRequestDTO.getUsername()),authRequestDTO.getUsername(),roles);
+            Utilisateur user = userRepository.findByUsername(authRequestDTO.getUsername());
+            // Récupérer les rôles de l'utilisateur
+            List<String> roles = authentication.getAuthorities().stream()
+                    .map(GrantedAuthority::getAuthority)
+                    .collect(Collectors.toList());
+
+            // Générer le token avec les rôles
+            String token = jwtService.generateToken(authRequestDTO.getUsername(),user.getId(), roles);
+            return new AuthResponseDTO(token);
         } else {
             throw new UsernameNotFoundException("invalid user request..!!");
         }
     }
 }
+
