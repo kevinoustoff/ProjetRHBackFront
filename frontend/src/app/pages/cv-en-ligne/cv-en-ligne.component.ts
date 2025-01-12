968c2067bf3ec403543f21224f229ccb102eeef3
Sami <samiboulsnane68@gmail.com>
2025-01-12 19:28:14 +0100

    Ajout de la création de cv en ligne (ne marche pas encore)

---
+++ frontend/src/app/pages/cv-en-ligne/cv-en-ligne.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import {CvEnLigne} from "../../models/CvEnLigne";
+import {CvEnLigneService} from "../../services/cv-en-ligne.service";
+import {AuthService} from "../../services/auth.service";
+import {MenuComponent} from "../../layout/menu/menu.component";
+import {CommonModule} from "@angular/common";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+
+@Component({
+  selector: 'app-cv-en-ligne',
+  standalone: true,
+  imports: [
+    MenuComponent, CommonModule, ReactiveFormsModule, FormsModule
+  ],
+  templateUrl: './cv-en-ligne.component.html',
+  styleUrl: './cv-en-ligne.component.scss'
+})
+export class CvEnLigneComponent {
+  cv: CvEnLigne | null = null;
+  isLoading: boolean = true;
+  constructor(private cvService: CvEnLigneService, private authService: AuthService) { }
+
+  ngOnInit(): void {
+    const userId = this.authService.getId();
+    this.cvService.getCvByUserId(userId).subscribe({
+      next: (data) => {
+        this.cv = data;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error('Erreur lors du chargement du CV:', err);
+        this.isLoading = false;
+      }
+    });
+  }
+
+}
